cd3a6109ebb269ac793715eb20f2f8fd542b83c6
PowerKuu <hkleven6@outlook.com>
2022-12-02 14:13:56 +0100

    update

--- server.ts
+++ server.ts
@@ -9,25 +9,43 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "/public")))
 
+const lenght = {
+    name: 20,
+    content: 20
+}
+
 app.get("/ping", async (req: Request, res: Response) => {
     res.send("KildeDB - V1")
 })
 
 
 app.post("/api/addCategory", async (req: Request, res: Response) => {
-    const db = await database.addCategory(req.body.categoryName)
+    const name = req.body.categoryName
+
+    if (!name || name.lenght > lenght.name) {
+        res.sendStatus(401)
+        return
+    }
+
+    const db = await database.addCategory(name)
     if (db) res.sendStatus(200)
     else res.sendStatus(400)
 })
 
 app.post("/api/addSource", async (req: Request, res: Response) => {
-    const db = await database.addSource(req.body.categoryID, req.body.sourceName)
+    const name = req.body.sourceName
+
+    if (!name || name.lenght > lenght.name) {
+        res.sendStatus(401)
+        return
+    }
+    
+    const db = await database.addSource(req.body.categoryID, name)
     if (db) res.sendStatus(200)
     else res.sendStatus(400)
 })
 
 app.post("/api/getCategories", async (req: Request, res: Response) => {
-
     const db = await database.getCategories()
     if (db) res.json(db)
     else res.sendStatus(400)
@@ -60,7 +78,14 @@ app.post("/api/removeSource", async (req: Request, res: Response) => {
 })
 
 app.post("/api/updateSourceContent", async (req: Request, res: Response) => {
-    const db = await database.updateSourceContent(req.body.sourceID, req.body.content)
+    const content = req.body.content
+
+    if (!content || content.lenght > lenght.content) {
+        res.sendStatus(401)
+        return
+    }
+    
+    const db = await database.updateSourceContent(req.body.sourceID, content)
     if (db) res.send(db)
     else res.sendStatus(400)
 })
@@ -68,14 +93,28 @@ app.post("/api/updateSourceContent", async (req: Request, res: Response) => {
 
 
 app.post("/api/searchCategories", async (req: Request, res: Response) => {
-    const db = await database.searchCategories(req.body.search)
+    const name = req.body.search
+
+    if (!name || name.lenght > lenght.name) {
+        res.sendStatus(401)
+        return
+    }
+    
+    const db = await database.searchCategories(name)
     if (db) res.json(db)
     else res.sendStatus(400)
 })
 
 
 app.post("/api/searchSources", async (req: Request, res: Response) => {
-    const db = await database.searchSources(req.body.categoryID, req.body.search)
+    const name = req.body.search
+
+    if (!name || name.lenght > lenght.name) {
+        res.sendStatus(401)
+        return
+    }
+
+    const db = await database.searchSources(req.body.categoryID, name)
     if (db) res.json(db)
     else res.sendStatus(400)
 })
